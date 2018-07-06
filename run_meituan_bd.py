7aac9dbbc0db8a7c70b5a9e7883b389564548280
陆翊司 <gamedebug@vip.qq.com>
2018-07-07 03:18:06 +0800

    add company

---
+++ run_meituan_bd.py
@@ -0,0 +1,60 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# Executable script, created by
+# Louis (luyisi@1000cc.net) on Sat Jul  7 1:32:26 2018.
+
+#!/usr/bin/env python
+# coding=utf-8
+from liepin_spider import LiepinSpider
+import sys
+import xlwt
+import  string
+import pandas as pd
+import codecs
+
+
+spider = LiepinSpider()
+
+url = 'https://www.liepin.com/zhaopin/?pubTime=&ckid=c99d18de175b6fcc&fromSearchBtn=2&compkind=&searchType=1&dqs=&compIds=2036768&jobKind=&sortFlag=15&salary=&key=%E5%A4%A7%E6%95%B0%E6%8D%AE&headckid=b477f3060083bfdc&isAnalysis=&init=-1&industryType=&degradeFlag=0&industries=&compscale=&clean_condition=&d_pageSize=40&siTag=LGV-fc5u_67LtFjetF6ACg~fA9rXquZc5IkJpXC-Ycixw&d_headId=a9a54a63b6466b81a581384304f1773f&d_ckId=a9a54a63b6466b81a581384304f1773f&d_sfrom=search_prime&d_curPage=0&curPage=0'
+
+url_head = 'https://www.liepin.com/zhaopin/?pubTime=&ckid=c99d18de175b6fcc&fromSearchBtn=2&compkind=&searchType=1&dqs=&compIds=2036768&jobKind=&sortFlag=15&salary=&key=%E5%A4%A7%E6%95%B0%E6%8D%AE&headckid=b477f3060083bfdc&isAnalysis=&init=-1&industryType=&degradeFlag=0&industries=&compscale=&clean_condition=&d_pageSize=40&siTag=LGV-fc5u_67LtFjetF6ACg~fA9rXquZc5IkJpXC-Ycixw&d_headId=a9a54a63b6466b81a581384304f1773f&d_ckId=a9a54a63b6466b81a581384304f1773f&d_sfrom=search_prime&d_curPage=0&curPage='
+
+def excel_write(items,index):
+    for item in items:
+        for i in range(0,5):
+            ws.write(index,i,item[i])
+        print(index)
+        index+=1
+
+
+newTable="meituan.xls"
+wb = xlwt.Workbook(encoding='utf-8')
+ws = wb.add_sheet('sheet1')
+headData = ['职位', '链接', '薪资', '发布时间', '公司']
+
+for colnum in range(0, 5):
+    ws.write(0, colnum, headData[colnum], xlwt.easyxf('font: bold on'))
+
+index = 1
+items = []
+for page_num in range(0, spider.page_num(url)):
+    url = url_head + str(page_num)
+    items += spider.get(spider.get_content(url))
+
+excel_write(items,index)
+wb.save(newTable)
+
+xd = pd.ExcelFile('meituan.xls')
+df = xd.parse()
+with codecs.open('meituan.html','w','gb2312') as html_file:
+        html_file.write(df.to_html(header = True,index = False))
