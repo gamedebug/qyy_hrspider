1c503dd1e7f3f5a60235c2912b18a028c0df8579
陆翊司 <gamedebug@vip.qq.com>
2018-07-08 16:57:50 +0800

    fixed bug

--- run.sh
+++ run.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "================================"
 echo "=         开始抓取任务         ="
 echo "================================"
 echo "任务一开始..."
-python run_ali_bd.py
+python3 run_ali_bd.py
 echo "任务一完成!!!"
 echo "任务二开始..."
-python run_baidu_bd.py
+python3 run_baidu_bd.py
 echo "任务二完成!!!"
 echo "任务三开始..."
-python run_didi_bd.py
+python3 run_didi_bd.py
 echo "任务三完成!!!"
 echo "任务四开始..."
-python run_huawei_bd.py
+python3 run_huawei_bd.py
 echo "任务四完成!!!"
 echo "任务五开始..."
-python run_jd_bd.py
+python3 run_jd_bd.py
 echo "任务五完成!!!"
 echo "任务六开始..."
-python run_meituan_bd.py
+python3 run_meituan_bd.py
 echo "任务六完成!!!"
 echo "任务七开始..."
-python run_tencent_bd.py
+python3 run_tencent_bd.py
 echo "任务七完成!!!"
